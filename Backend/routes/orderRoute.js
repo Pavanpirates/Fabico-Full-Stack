91da0b77710008eca2663f8cba7c948f176abc76
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-11-06 03:44:28 +0530

    push3

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,22 +1,22 @@
 import express from 'express';
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateSatus} from '../controllers/orderController.js';
+import {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateSatus
+} from '../controllers/orderController.js';
 import adminAuth from '../middleware/adminAuth.js';
 import userAuth from '../middleware/auth.js';
 
+const orderRouter = express.Router();
 
-const router = express.Router();
-
-// Admin features
-orderRouter.post('/list',adminAuth, allOrders);
-orderRouter.post('/satus',adminAuth, updateSatus);
-
-
-// User features
-orderRouter.post('/place',userAuth, placeOrder);
-orderRouter.post('/stripe',userAuth, placeOrderStripe);
-orderRouter.post('/razorpay',userAuth, placeOrderRazorpay);
-
-// User features
-orderRouter.post('/userorders',userAuth, userOrders);
+orderRouter.post('/list', adminAuth, allOrders);
+orderRouter.post('/status', adminAuth, updateSatus);
+orderRouter.post('/place', userAuth, placeOrder);
+orderRouter.post('/stripe', userAuth, placeOrderStripe);
+orderRouter.post('/razorpay', userAuth, placeOrderRazorpay);
+orderRouter.post('/userorders', userAuth, userOrders);
 
 export default orderRouter;
